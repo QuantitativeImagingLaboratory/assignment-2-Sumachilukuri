d8a1a4514b39338d8dfbfc6c3e641756bf266818
Sumachilukuri <suma.ch1411@gmail.com>
2017-11-01 20:31:12 -0500

    filter

--- dip_hw2_filter.py
+++ dip_hw2_filter.py
@@ -89,11 +89,11 @@ def main():
     output_dir = 'output/'
 
     output_image_name = output_dir+image_name+"_"+mask+datetime.now().strftime("%m%d-%H%M%S")+".jpg"
-    #cv2.imwrite(output_image_name, output[0])
+    cv2.imwrite(output_image_name, output[0])
     output_image_name = output_dir + image_name+"_dft_" + mask + datetime.now().strftime("%m%d-%H%M%S") + ".jpg"
-    #cv2.imwrite(output_image_name, output[1])
+    cv2.imwrite(output_image_name, output[1])
     output_image_name = output_dir + image_name + "_dft_filter_" + mask + datetime.now().strftime("%m%d-%H%M%S") + ".jpg"
-    #cv2.imwrite(output_image_name, output[2])
+    cv2.imwrite(output_image_name, output[2])
 
 
 if __name__ == "__main__":
